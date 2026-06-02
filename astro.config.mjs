94f4aceb490ce441e4ab9c621ad0ec59ed169366
Vaše Meno <stancikmarian8@gmail.com>
2026-06-02 11:44:19 +0200

    Configure Vercel adapter and SSR output

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,17 +4,16 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import svelte from '@astrojs/svelte';
 
-import node from '@astrojs/node';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
   vite: {
     plugins: [tailwindcss()]
   },
 
   integrations: [svelte()],
 
-  adapter: node({
-    mode: 'standalone'
-  })
+  adapter: vercel()
 });
